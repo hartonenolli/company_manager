a581f5a7dfe0efa56db40ac42769e14bb11deb69
hartonenolli <hartonenolli@gmail.com>
2023-02-03 22:00:34 +0200

    Succesful use of secret key

--- routes.py
+++ routes.py
@@ -1,13 +1,22 @@
 from app import app
-from flask import render_template, request
+from flask import render_template, request, redirect, session
 import database_methods
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/login", methods=["POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    user = request.form["name"]
+    if request.method == "GET":
+        user = session["username"]
+    else:
+        user = request.form["name"]
+        session["username"] = user
     admin = database_methods.get_admin(user)
     return render_template("login.html", user=user, admin=admin)
+
+@app.route("/search")
+def search():
+
+    return render_template("search.html")
