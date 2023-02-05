1da6403d58f5d8f9ee59a6fa19918d66398a2e02
hartonenolli <hartonenolli@gmail.com>
2023-02-05 11:52:00 +0200

    New work form

--- routes.py
+++ routes.py
@@ -16,10 +16,27 @@ def login():
     admin = database_methods.get_admin(user)
     return render_template("login.html", user=user, admin=admin)
 
+@app.route("/login_added_new_work", methods=["POST"])
+def login_add():
+    #costumer = request.form["costumer"]
+    #work_type = request.form["work_type"]
+    #price = request.form["price"]
+    #status = request.form["status"]
+    #start_date = request.form["start_date"]
+    #TO DO
+    user = session["username"]
+    admin = database_methods.get_admin(user)
+    return render_template("login.html", user=user, admin=admin)
+
 @app.route("/search")
 def search():
 
     return render_template("search.html")
+
+@app.route("/add")
+def add():
+
+    return render_template("add.html")
 
 @app.route("/info", methods=["POST"])
 def info():
