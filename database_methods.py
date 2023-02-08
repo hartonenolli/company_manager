07fd8fc09220498ab49de683d99f6b5382494649
hartonenolli <hartonenolli@gmail.com>
2023-02-08 14:15:18 +0200

    Visited paja, fixed problem with sqlsearches

--- database_methods.py
+++ database_methods.py
@@ -1,17 +1,18 @@
 from db import db
+from sqlalchemy.sql import text
 
 def get_admin(username):
-    result = db.session.execute("SELECT admin FROM users WHERE username=:username", {'username':username})
+    result = db.session.execute(text("SELECT admin FROM users WHERE username=:username"), {'username':username})
     admin = result.fetchone()
     if admin:
         return "You are admin"
     return "You are not admin"
 
 def get_count_by_costumer(username):
-    id_result = db.session.execute("SELECT id FROM users WHERE username=:username", {'username':username})
+    id_result = db.session.execute(text("SELECT id FROM users WHERE username=:username"), {'username':username})
     id = id_result.fetchone()
     if id is None:
         return 0
-    result = db.session.execute("SELECT COUNT(costumer) FROM work WHERE user_id=:id", {'id':id[0]})
+    result = db.session.execute(text("SELECT COUNT(costumer) FROM work WHERE user_id=:id"), {'id':id[0]})
     count = result.fetchone()[0]
     return count
